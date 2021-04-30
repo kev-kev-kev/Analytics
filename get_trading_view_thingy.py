81c0ce4d8d1a70ff5c02506b776b868cb85bc82c
kevinboekholt <kevin_boekholt@outlook.com>
2021-04-30 22:25:48 +0200

    Getting started loop w/ Binance

--- get_trading_view_thingy.py
+++ get_trading_view_thingy.py
@@ -1,34 +1,37 @@
 from pycoingecko import CoinGeckoAPI
-from FTX_api import FtxClient
+from utilities import get_data_binance
+from pandas import DataFrame, read_csv
+from datetime import date, timedelta
 
-cg = CoinGeckoAPI()
-ft = FtxClient()
+
+init_df = read_csv("CEX_tokens.txt", names=["Token"])
+init_df[["Exchange", "Token"]] = init_df["Token"].str.split(':', expand=True)
+init_df = init_df[["Exchange", "Token"]]
+
+
+
+for index, rows in init_df.iterrows():
+    current_exchange = rows["Exchange"]
+    current_token = rows["Token"]
+
+    current_date = date.today()
+    init_date = current_date - timedelta(days=7)
+
+    current_date = current_date.strftime("%d-%m-%Y")
+    init_date = init_date.strftime("%d-%m-%Y")
+    
+    if(current_exchange == "Binance"):
+        # get history, get volatility, add to dataframe. 
+        df = get_data_binance(current_token, "1d", init_date)
+        print(df)
+
+
+
+
 
 
 coin_list = ["BNBUSDT",
             "FTTUSDT"]
-
-
-# /simple/price endpoint with the required parameters
->>> cg.get_price(ids='bitcoin', vs_currencies='usd')
-{'bitcoin': {'usd': 3462.04}}
-
->>> cg.get_price(ids='bitcoin,litecoin,ethereum', vs_currencies='usd')
-# OR (lists can be used for multiple-valued arguments)
->>> cg.get_price(ids=['bitcoin', 'litecoin', 'ethereum'], vs_currencies='usd')
-{'bitcoin': {'usd': 3461.27}, 'ethereum': {'usd': 106.92}, 'litecoin': {'usd': 32.72}}
-
->>> cg.get_price(ids='bitcoin,litecoin,ethereum', vs_currencies='usd,eur')
-# OR (lists can be used for multiple-valued arguments)
->>> cg.get_price(ids=['bitcoin', 'litecoin', 'ethereum'], vs_currencies=['usd', 'eur'])
-{'bitcoin': {'usd': 3459.39, 'eur': 3019.33}, 'ethereum': {'usd': 106.91, 'eur': 93.31}, 'litecoin': {'usd': 32.72, 'eur': 28.56}}
-
-# optional parameters can be passed as defined in the API doc (https://www.coingecko.com/api/docs/v3)
->>> cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
-{'bitcoin': {'usd': 3458.74, 'usd_market_cap': 60574330199.29028, 'usd_24h_vol': 4182664683.6247883, 'usd_24h_change': 1.2295378479069035, 'last_updated_at': 1549071865}}
-# OR (also booleans can be used for boolean type arguments)
->>> cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap=True, include_24hr_vol=True, include_24hr_change=True, include_last_updated_at=True)
-{'bitcoin': {'usd': 3458.74, 'usd_market_cap': 60574330199.29028, 'usd_24h_vol': 4182664683.6247883, 'usd_24h_change': 1.2295378479069035, 'last_updated_at': 1549071865}}
 
 1. get prices of all for last week
 2. get volatiliy
--- utilities.py
+++ utilities.py
@@ -1,6 +1,14 @@
 # Utilities
 from pandas import DataFrame
 
+from time import time, mktime
+from datetime import datetime
+import urllib.parse
+from typing import Optional, Dict, Any, List
+
+from requests import Request, Session, Response
+import hmac
+
 def get_data_binance(symbol: str, kline_size: str, init_date: str, save=False) -> DataFrame:
     """ This function downloads the data available for the given symbol and
     given timeframe. It also writes the data to the computer if it is
@@ -10,7 +18,7 @@ def get_data_binance(symbol: str, kline_size: str, init_date: str, save=False) -
     :param kline_size str: The timeframe that we want to download the data.
     :param save bool:      A boolean denoting whether we require the data
                            to be saved. Default is False.
-    :param init_date str: "1 Jan 2020" <- how it should look like
+    :param init_date str: "01-01-2020" <- how it should look like
     :return data_df: A dataframe containing all the data corresponding
                      to the coin.
     :rtype: DataFrame
@@ -27,7 +35,7 @@ def get_data_binance(symbol: str, kline_size: str, init_date: str, save=False) -
         symbol=symbol, interval=kline_size)[-1][0], unit='ms')
     newest_point = newest_point.strftime("%d %b %Y %H:%M:%S")
     # oldest_point = datetime.strptime('1 Jan 2017', '%d %b %Y')
-    oldest_point = datetime.strptime(init_date, '%d %b %Y')
+    oldest_point = datetime.strptime(init_date, "%d-%m-%Y")
     oldest_point = oldest_point.strftime("%d %b %Y %H:%M:%S")
 
     filename = (CURRENT_PATH +
@@ -67,4 +75,69 @@ def get_data_binance(symbol: str, kline_size: str, init_date: str, save=False) -
 
     return data_df
 
-# Get past two weeks
+def str_to_EPOX(date: str) -> int:
+    epox_int = mktime(datetime.strptime(date, "%d-%m-%Y").timetuple())
+    epox_int = int(epox_int)
+    return epox_int
+
+# From: https://github.com/quan-digital/ftx/blob/v1.1/ftx/api.py
+class FtxClient:
+
+    _ENDPOINT = 'https://ftx.com/api/'
+    FTX_key = "tJiM28KoZUBb_ESuw9bLSCCKesm0xQ8kEX4gAthO"
+    FTX_secret = "BIAT0328EvQhP4n_-ZoX44rwSSO15XAGEDLpnUW0"
+
+    def __init__(self, base_url=None, api_key=None, api_secret=None, subaccount_name=None) -> None:
+        self._session = Session()
+        self._api_key = FTX_key
+        self._api_secret = FTX_secret
+        self._subaccount_name = subaccount_name
+
+    def _get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('GET', path, params=params)
+
+    def _post(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('POST', path, json=params)
+
+    def _delete(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('DELETE', path, json=params)
+
+    def _request(self, method: str, path: str, **kwargs) -> Any:
+        request = Request(method, self._ENDPOINT + path, **kwargs)
+        self._sign_request(request)
+        response = self._session.send(request.prepare())
+        return self._process_response(response)
+
+    def _sign_request(self, request: Request) -> None:
+        ts = int(time() * 1000)
+        prepared = request.prepare()
+        signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode()
+        if prepared.body:
+            signature_payload += prepared.body
+        signature = hmac.new(self._api_secret.encode(), signature_payload, 'sha256').hexdigest()
+        request.headers['FTX-KEY'] = self._api_key
+        request.headers['FTX-SIGN'] = signature
+        request.headers['FTX-TS'] = str(ts)
+        if self._subaccount_name:
+            request.headers['FTX-SUBACCOUNT'] = urllib.parse.quote(self._subaccount_name)
+
+    def _process_response(self, response: Response) -> Any:
+        try:
+            data = response.json()
+        except ValueError:
+            response.raise_for_status()
+            raise
+        else:
+            if not data['success']:
+                raise Exception(data['error'])
+            return data['result']
+
+
+    # start_time + resolution
+    def get_historical_data(self, market_name: str, 
+                            resolution: int, 
+                            limit: int, 
+                            start_time: float, 
+                            end_time: float) -> dict:
+        return self._get(f'markets/{market_name}/candles', dict(resolution=resolution,limit=limit,start_time=start_time,end_time=end_time))
+
